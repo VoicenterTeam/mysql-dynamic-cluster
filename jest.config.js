1b88042bb0f9069b261dc4d5c5ea97a8bce9d95c
Bohdan <bomtvv@gmail.com>
2021-09-09 12:09:38 +0300

    configure jest tests for typescript

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleDirectories: [
+    "node_modules"
+  ]
+};
