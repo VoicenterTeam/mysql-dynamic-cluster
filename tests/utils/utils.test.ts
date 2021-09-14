9e9a8dadbccfdea408d5d44c5e7d6d746596ebd2
Bohdan <bomtvv@gmail.com>
2021-09-14 11:50:13 +0300

    fix utils test

--- tests/utils/utils.test.ts
+++ tests/utils/utils.test.ts
@@ -1,18 +1,15 @@
 import { Timer } from "../../src/utils/Timer";
-import {Utils} from "../../src/utils/Utils";
+import { Utils } from "../../src/utils/Utils";
 
-test("Timer", async (done) => {
+test("Timer async", async () => {
     let count = 0;
     const timer = new Timer(() => {
-        count++;
         console.log("Count")
-        timer.start(30)
-    });
+        count++;
+        timer.start(30);
+    })
     timer.start(30);
-    const utils = new Utils();
+    await new Utils().sleep(35 * 4);
 
-    await utils.sleep(35 * 4);
-    timer.dispose();
-    expect(count).toBe(3);
-    done();
+    await expect(count).toBe(3);
 })
