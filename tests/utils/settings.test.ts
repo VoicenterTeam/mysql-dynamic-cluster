28e6cbd3be23461237accc2b960359c57b5c979a
Bohdan <bomtvv@gmail.com>
2021-09-29 17:22:12 +0300

    add mix pool settings test

---
+++ tests/utils/settings.test.ts
@@ -0,0 +1,57 @@
+/**
+ * Created by Bohdan on Sep, 2021
+ */
+
+import { Settings } from "../../src/Settings";
+import { PoolSettings, UserSettings } from "../../src/types/SettingsInterfaces";
+
+describe("Mix settings", () => {
+    it ("Set credentials for all pools", () => {
+        const userSettings: UserSettings = {
+            hosts: [
+                {
+                    host: '192.168.0.1'
+                }
+            ],
+            user: 'test',
+            password: 'test2',
+            database: 'db'
+        }
+
+        const expectedResult: PoolSettings = {
+            host: '192.168.0.1',
+            user: 'test',
+            password: 'test2',
+            database: 'db'
+        }
+
+        const result = Settings.mixPoolSettings(userSettings.hosts[0], userSettings);
+
+        expect(result).toStrictEqual(expectedResult);
+    })
+
+    it('Override global credential for pool', () => {
+        const userSettings: UserSettings = {
+            hosts: [
+                {
+                    host: '192.168.0.1',
+                    user: 'amazing'
+                }
+            ],
+            user: 'test',
+            password: 'test2',
+            database: 'db'
+        }
+
+        const expectedResult: PoolSettings = {
+            host: '192.168.0.1',
+            user: 'amazing',
+            password: 'test2',
+            database: 'db'
+        }
+
+        const result = Settings.mixPoolSettings(userSettings.hosts[0], userSettings);
+
+        expect(result).toStrictEqual(expectedResult);
+    })
+})
